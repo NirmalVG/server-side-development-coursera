aebea8ff992abdea488ac78c3178ec2a1bb657a0
Nirmal V G <nirmalvg97@gmail.com>
2022-02-15 15:59:42 +0530

    Basic Authentication

--- app.js
+++ app.js
@@ -10,16 +10,21 @@ var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Dishes = require('./models/dishes');
+const Dishes = require("./models/dishes");
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = "mongodb://localhost:27017/conFusion";
 const connect = mongoose.connect(url);
 
-connect.then((db) => {
-    console.log("Connected correctly to server");
-}, (err) => { console.log(err); });
+connect.then(
+    (db) => {
+        console.log("Connected correctly to server");
+    },
+    (err) => {
+        console.log(err);
+    }
+);
 
 var app = express();
 
@@ -31,6 +36,35 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req, res, next) {
+    console.log(req.headers);
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error("You are not authenticated!");
+        res.setHeader("WWW-Authenticate", "Basic");
+        err.status = 401;
+        next(err);
+        return;
+    }
+
+    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+        .toString()
+        .split(":");
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == "admin" && pass == "password") {
+        next(); // authorized
+    } else {
+        var err = new Error("You are not authenticated!");
+        res.setHeader("WWW-Authenticate", "Basic");
+        err.status = 401;
+        next(err);
+    }
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
