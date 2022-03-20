25de391cc5c0b5240a5a6daf0bed978bfb3dcdfc
Nirmal V G <nirmalvg97@gmail.com>
2022-03-20 11:42:14 +0530

    Uploading Files

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var users = require("./routes/users");
 var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
+const uploadRouter = require('./routes/uploadRouter');
 var config = require("./config");
 
 const mongoose = require("mongoose");
@@ -64,6 +65,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 app.use(function (req, res, next) {
     var err = new Error("Not Found");
