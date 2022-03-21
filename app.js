0d6b2a501a8f70eaf10d7920fc399ab0b7bcc8f4
Nirmal V G <nirmalvg97@gmail.com>
2022-03-21 11:59:25 +0530

    Assignment 4

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var users = require("./routes/users");
 var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
-const uploadRouter = require('./routes/uploadRouter');
+const uploadRouter = require("./routes/uploadRouter");
+const favoriteRouter = require("./routes/favoriteRouter");
 var config = require("./config");
 
 const mongoose = require("mongoose");
@@ -24,7 +25,9 @@ var passport = require("passport");
 var authenticate = require("./authenticate");
 
 const url = config.mongoUrl;
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url, {
+    useMongoClient: true,
+});
 
 connect.then(
     (db) => {
@@ -65,7 +68,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
-app.use('/imageUpload',uploadRouter);
+app.use("/imageUpload", uploadRouter);
+app.use("/favorites", favoriteRouter);
 
 app.use(function (req, res, next) {
     var err = new Error("Not Found");
